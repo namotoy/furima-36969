c3945eae620aa157d8855203a576841ecfbedb8a
namotoy <namotopet0615@gmail.com>
2021-12-30 21:00:16 +0900

    ユーザー管理機能　レビュー依頼２

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-  validates :last_name, presence: true,format: {with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name, presence: true,format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :last_name, presence: true,format: {with:  /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :first_name, presence: true,format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :last_name_kana, presence: true,format: {with: /\A[ァ-ヶー－]+\z/ }
   validates :first_name_kana, presence: true,format: {with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday, presence: true
