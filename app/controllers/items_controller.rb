e531d42cca6964745c2a1a9b25f94531f86ad7c0
namotoy <namotopet0615@gmail.com>
2022-01-08 10:47:33 +0900

    ActiveHash導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,22 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only:[:new, :edit, :destroy] 
+  def new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :explanation, :category_id, :situation_id,:delively_charge_id, :prefecture_id, :shipping_days_id)
+  end
+  
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,13 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :situation
+  belongs_to :delively_charge
+  belongs_to :prefecture
+  belongs_to :shipping_days
+
+  validates :title, :explanation, presence: true 
+
+  validates :category_id, :situation_id,:delively_charge_id, :prefecture_id, :shipping_days_id, numericality: {other_than: 0 , message: "can't be blank"}
 end
