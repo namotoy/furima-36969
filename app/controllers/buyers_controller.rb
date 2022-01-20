8d7462069706b0f6bfb4ad4beadfd9481c42147a
namotoy <namotopet0615@gmail.com>
2022-01-20 21:43:03 +0900

    フォーム情報の保存

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -2,10 +2,23 @@ class BuyersController < ApplicationController
 
   def index
     @buyer_address = BuyerAddress.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
-    
+    @buyer_address = BuyerAddress.new(buyer_params)
+    if @buyer_address.valid?
+      @buyer_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def buyer_params
+    params.require(:buyer_address).permit(:postal_code, :prefecture, :city, :block_number, :building_name, :telephone_number, :price, :image).merge(user_id: current_user.id)
   end
 
 
--- app/models/buyer_address.rb
+++ app/models/buyer_address.rb
@@ -1,14 +1,15 @@
 class BuyerAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture, :city, :block_number, :building_name,:telephone_number,:user_id
-
+with_options presence: true do
   validates :postal_code, :prefecture, :city, :block_number, :telephone_number, presence: true
   validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
   validates :telephone_number, numericality: {with: /\A{10,11}\z/, message: "is invalid.Input half-width characters"}
+end
+validates :prefecture, numericality: { other_than: 0, message: "can't be blank" }
 
   def save
     buyer = Buyer.create(item_id: item_id, user_id: user_id)
-    Address.create(postal_code: postal_code, prefecture: prefecture, city: city, block_number: block_number, building_name: building_name, :telephone_number: telephone_number,buyer_id: buyer.id)
+    Address.create(postal_code: postal_code, prefecture: prefecture, city: city, block_number: block_number, building_name: building_name, telephone_number: telephone_number,buyer_id: buyer.id)
   end
 end
