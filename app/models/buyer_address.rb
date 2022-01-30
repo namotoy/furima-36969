9220f4526224fc02b6ec57ce839e560d44728ef6
namotoy <namotopet0615@gmail.com>
2022-01-30 11:19:37 +0900

    商品機能レビュー修正３

--- app/models/buyer_address.rb
+++ app/models/buyer_address.rb
@@ -5,10 +5,9 @@ class BuyerAddress
   with_options presence: true do
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :prefecture_id, :city, :block_number, :user_id, :item_id, :token
-    validates :telephone_number, numericality: { with: /\A{10,11}\z/, message: 'is invalid.Input half-width characters' }
+    validates :telephone_number, format: { with: /\A\d{10,11}\z/,message: 'is invalid.Input half-width characters' }
   end
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
-  validates :telephone_number, format: { with: /\A\d{10,11}\z/ }
 
   def save
     buyer = Buyer.create(item_id: item_id, user_id: user_id)
